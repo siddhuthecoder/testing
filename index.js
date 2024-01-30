82f5edf63319a245fdc5de90ec22be98bb777ba3
siddhu_from_sklm <codewithsiddhu@gmail.com>
2024-01-30 21:28:21 +0530

    first

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
-console.log('='.repeat(50))
-   console.log("hi this is siddhu from srikakulam")
-console.log('='.repeat(50))
-console.log("hi this is siddhu from srikakilam");
+import { ethers } from "./ethers-5.2.esm.min.js";
+
+
+const connectButton=document.getElementById("connectButton");
+const fundButton=document.getElementById("fundButton");
+connectButton.onclick=connect;
+fundButton.onclick=fund;
+
+
+async function connect() {
+  if (typeof window.ethereum != undefined) {
+    await window.ethereum.request({ method: "eth_requestAccounts" });
+    connectButton.innerHTML = "Connected";
+  } else {
+    console.log("NO I cant see metamask");
+    connectButton.innerHTML =
+      "Please Install MetaMask";
+  } 
+
+}
+async function fund(ethAmount){
+   console.log(`Funding with ${ethAmount}...`);
+   if (typeof window.ethereum!=="undefined ") {
+      
+   }
+}
+ 
